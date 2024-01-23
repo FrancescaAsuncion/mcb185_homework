5b659358e9bdb757b0a836f2694c7961fc77d0b8
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-23 09:36:10 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -4,4 +4,6 @@ gunzip -c ~/code/MCB185/data/dictionary.gz | grep "a" | grep -v "[^auotfcm]" | g
 gunzip -c ~/code/MCB185/data/dictionary.gz | grep "b" | grep -v "[^bailnrt]" | grep -E ".{4,}" | wc
 gunzip -c ~/code/MCB185/data/dictionary.gz | grep "c" | grep -v "[^caonidm]" | grep -E ".{4,}" | wc
 gunzip -c ~/code/MCB185/data/dictionary.gz | grep "z" | grep -v "[^znorgia]" | grep -E ".{4,}" | wc
+gunzip -c ~/code/MCB185/data/jaspar2024_core.transfac.gz | grep tax | cut -d ":" -f 2 | sort | uniq -c | sort -nr
+print("Co-authors: Anisha Patel and Varsha Thalladi")
 
