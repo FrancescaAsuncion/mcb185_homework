c776fc5d3fe7a1e4846192f5eb60197c20837a6e
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-23 09:14:04 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -1,3 +1,7 @@
 print("Franceca_Asuncion")
 print("francescaasuncion")
 gunzip -c ~/code/MCB185/data/dictionary.gz | grep "a" | grep -v "[^auotfcm]" | grep -E ".{4,}" | wc
+gunzip -c ~/code/MCB185/data/dictionary.gz | grep "b" | grep -v "[^bailnrt]" | grep -E ".{4,}" | wc
+gunzip -c ~/code/MCB185/data/dictionary.gz | grep "c" | grep -v "[^caonidm]" | grep -E ".{4,}" | wc
+gunzip -c ~/code/MCB185/data/dictionary.gz | grep "z" | grep -v "[^znorgia]" | grep -E ".{4,}" | wc
+
