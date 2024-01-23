b6e2bd10fb324ee616beedf76ae34f418db7583c
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-23 09:11:43 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -1,2 +1,3 @@
-Franceca Asuncion
-francescaasuncion
+print("Franceca_Asuncion")
+print("francescaasuncion")
+gunzip -c ~/code/MCB185/data/dictionary.gz | grep "a" | grep -v "[^auotfcm]" | grep -E ".{4,}" | wc
