79aefa099f55374b2d33914e85bc83610faf0a21
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-26 12:04:57 -0800

    new

--- 24accuracy.py
+++ 24accuracy.py
@@ -2,13 +2,13 @@
 # Demonstrate your function works by using it several times in the program.
 
 def test(tp, fp, tn, fn):
+	if tp <=0: return "error read"
 	precision = tp / (tp + fp)
 	recall = tp / (tp + fn)
+	if precision + recall <=0: "error read"
 	accuracy = (tp + tn) / (tp + fp + fn + tn)
 	f1 = (2 * precision * recall) / (precision + recall)
-	if tp <=0: return "error read"
-	if precision + recall <=0: "error read"
 	return f1, accuracy
 print(test(1,2,3,4))
-print(test(3, 10, 3, 2))
+print(test(0, 10, 3, 2)) #when I make tp = 0 I get terminal error
 print(test(4,3,2,1))
