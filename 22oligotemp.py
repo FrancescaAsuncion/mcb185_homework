83ada9ba8c7b38b9ff2443d3b7f75d34cde37862
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-26 11:08:45 -0800

    new

---
+++ 22oligotemp.py
@@ -0,0 +1,16 @@
+# Write a program that returns the oligo melting temperature given the number of As, Cs, Gs, and Ts in the oligo. Use these two methods.
+# For oligos <= 13 nt, Tm = (A+T)*2 + (G+C)*4
+# For longer oligos, Tm = 64.9 + 41*(G+C -16.4) / (A+T+G+C)
+# Demonstrate that your program works by computing the Tm of several oligos of different sizes.
+
+import math
+
+def oligos(A,C,G,T):
+	o_length = A + C + G + T
+	if o_length <= 13:
+		Tm = (A + T) * 2 + (G + C) * 4
+		return Tm
+	else:
+		Tm = 64.9 + 41 * (G + C - 16.4) / (A + T + G + C)
+		Return Tm
+print(oligos(10,10,10,10))
