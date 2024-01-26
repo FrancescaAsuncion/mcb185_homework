59b86a14e0726d9c9108a700a39e2d9e449cd7a5
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-25 21:26:02 -0800

    new

--- 21quadratic.py
+++ 21quadratic.py
@@ -8,10 +8,10 @@ import sys
 def quadratic_formula(a, b, c):
 	discriminant = (b**2 - 4*a*c)
 	if discriminant <0: sys.exit('error')
-		roots = math.sqrt(b**2 - 4*a*c)
-		square_1 = (-b + roots) / (2*a)
-		square_2 = (-b - roots) / (2*a)
-		return square_1, square_2
+	roots = math.sqrt(b**2 - 4*a*c)
+	square_1 = (-b + roots) / (2*a)
+	square_2 = (-b - roots) / (2*a)
+	return square_1, square_2
 
 print(quadratic_formula(30,4,1))
 print(quadratic_formula(50,4,2))
