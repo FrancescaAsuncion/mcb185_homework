cdf15c9802a0fa55300a7784a600d7adfbcbfe98
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-25 17:00:00 -0800

    unit2

---
+++ 21quadratic.py
@@ -0,0 +1,13 @@
+"""
+Write a function that solves the quadratic formula (ax^2 + bx + c), returning the two X-intercepts. 
+Demonstrate that it works by using the formula multiple times within the program.
+
+"""
+import math
+def quadratic_formula(a, b, c):
+	roots = math.sqrt(b**2 - 4*a*c)
+	b1 = (-b + roots) / (2*a)
+	b2 = (-b - roots) / (2*a)
+	return b1
+	return b2
+	print(quadratic_formula)
