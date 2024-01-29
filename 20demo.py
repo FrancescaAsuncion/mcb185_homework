9b0b282ccf82ebe6de0858dc49dac9c391735fa1
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-01-29 11:51:28 -0800

    new

---
+++ 20demo.py
@@ -0,0 +1,15 @@
+# 20demo.py by Francesca_Asuncion
+import math
+print ("hello, again") # greeting
+print(1.5e-2)
+print(5 % 2)
+print(5 * (2+1))
+print(math.sqrt(49))
+print(math.e)
+
+a = 3
+b = 4
+c = math.sqrt(a**2 + b**2)
+print(c)
+
+print(type(a), type(b), type(c), sep=',')
