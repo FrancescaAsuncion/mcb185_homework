3dd8410acbe77c8094931182af0ea8a635580eed
FrancescaAsuncion <fyasuncion@ucdavis.edu>
2024-02-06 09:33:37 -0800

    new

--- 38quiz.py
+++ 38quiz.py
@@ -1,4 +1,4 @@
-#coauthors: Lisa Yuan
+#coauthors: Lisa Yuan, Varsha Thalladi, Anisha Patel, Avantika
 
 def pi_calc(n):
 	for i in range(1,n):
@@ -7,4 +7,16 @@ def pi_calc(n):
 
 print(pi_calc(5))
 
-
+def nilakantha(iterations):
+	estimate_pi = 3
+	sign = 1
+	
+	for i in range(1, iterations):
+		denominator = 2 * i * (2 * i + 1) * (2 * i + 2)
+		now_add = 4 / denominator
+		estimate_pi += sign * now_add
+		sign *= -1
+		return estimate_pi
+		
+for i in range(1, 100):
+	print(pi_calc(i), nilakantha(i))
